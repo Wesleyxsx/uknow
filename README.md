d6b432bfc252c4d785bc22d11d6b4e401498a19f
rithienatan <rithienatan@gmail.com>
2022-07-27 16:18:31 -0300

    Init frontend React project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# UKnow
+# Uknow-frontend-indev
 
+React frontend app para o produto UKnow.
+
+## Como rodar a aplicação
+
+> npm i && npm start
---
+++ src/App.js
@@ -0,0 +1,18 @@
+//--------- Import styles ---------
+import './App.css';
+
+
+//---------- App ---------
+/**
+ * Root App
+ * 
+ * @returns {JSX}
+ */
+export default function App() 
+{
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+}//end App()
