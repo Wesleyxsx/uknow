a7422bee235ea9245fc08aab638a2fdbd65a88f9
Pedro <pilique11@gmail.com>
2022-07-28 07:31:29 -0300

    update file names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //--------- Import custom components ---------
 import Header from "./Components/header/index";
+import Footer from "./Components/footer/index";
 
 
 //--------- Import styles ---------
 import './App.css';
-import Footer from "./Components/footer/Footer";
 
 
 //---------- App ---------
--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -1,5 +1,5 @@
 //--------- Import styles ---------
-import './Footer.css';
+import './index.css';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
