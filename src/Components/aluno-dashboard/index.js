dd3fadbd23e6b514f5ca5d14469669d85df77584
Pedro <pilique11@gmail.com>
2022-07-29 15:36:23 -0300

    improve header

--- src/Components/aluno-dashboard/index.js
+++ src/Components/aluno-dashboard/index.js
@@ -1,18 +1,17 @@
 //--------- Import styles ---------
-import { Button, Container, Dropdown, Form } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import pic01 from './images/metalurgia.jpeg';
 import pic02 from './images/tosagem.jpeg';
+
+
+
 import Footer from '../footer';
+import Header2 from '../header2';
 
 
 //----------- Custom Style Import ---------
 import './index.css';
-
-
-import { FaUserCircle } from 'react-icons/fa'; 
-import { BsFillCaretDownFill } from 'react-icons/bs'; 
-import { AiOutlineBell } from 'react-icons/ai'; 
 
  
 
@@ -24,70 +23,9 @@ import { AiOutlineBell } from 'react-icons/ai';
  */
 export default function AlunoDashboard() {
 
-    function handleLogoClick(e) {
-        e.preventDefault();
-        window.location.href='http://google.com';
-    }
-
     return (
         <div className='aluno__dashboard'>
-            <header>
-                <div id='header__item__01' className='header__flex__item'>
-                    <img id='user__header__logo' className="logo" src='Logoamarelo.png' alt='Logo-amarelo' onClick={(e) => handleLogoClick(e)} />
-                    <span>&nbsp;</span>
-                    <span onClick={(e) => handleLogoClick(e)}>UKnow</span>
-                </div>
-
-                <div id='header__item__02' className='header__flex__item'>
-                    <div className='search__bar'>
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <div className='newsletter__form'>
-                                <Form.Control type="text" placeholder="pesquisar" />
-                                <Button variant="primary">
-                                    Enviar
-                                </Button>                                
-                            </div>                            
-                        </Form.Group>
-                    </Form>
-                    </div>
-                </div>
-
-                <div id='header__item__03' className='header__flex__item'>
-                    <span>00.00 tK</span>
-                    <div id='notif__bell'><AiOutlineBell />&nbsp;&nbsp;&nbsp;|</div>
-                </div>
-
-                <div id='header__item__04' className='header__flex__item'>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            <div id='menu__perfil'>
-                                <div id='nome__usuario'>
-                                    Armando P.
-                                </div>
-                                <div id='down__icon'>
-                                    &nbsp;<BsFillCaretDownFill />
-                                </div>
-                                <div>
-                                    <FaUserCircle id='pic__usuario' className='big__menu__obj'/>
-                                </div>
-                            </div>    
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Meu Dashboard</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Meu Perfil</Dropdown.Item>
-                            <Dropdown.Item href="#/action-1">Carrinho</Dropdown.Item>
-                            <Dropdown.Item href="#/action-1">Minha Conta</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Configurações</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>                    
-                </div>
-
-            </header>
-
-
-
+            <Header2 />
 
             <main className='dashboard__content'>
                 <section id='section__01'>
